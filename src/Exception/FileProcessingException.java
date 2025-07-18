12d7a05bb85321715de4d9be27e97ed533d4b21a
sVuMPaR <145699180+sVuMPaR@users.noreply.github.com>
2025-07-18 21:09:03 +0500

    Update FileProcessingException.java
    Add comments

--- src/Exception/FileProcessingException.java
+++ src/Exception/FileProcessingException.java
@@ -3,19 +3,22 @@ package Exceptions;
 public class FileProcessingException extends RuntimeException {
     private final String defaultFilePath;
 
-    // Constructor for message and default file path
+    // Constructor for message and defaultFilePath
+    // Конструктор для message и defaultFilePath
     public FileProcessingException(String message, String defaultFilePath) {
         super(message);
         this.defaultFilePath = defaultFilePath;
     }
 
     // Constructor for just a message
+    // Конструктор для message
     public FileProcessingException(String message) {
         super(message);
         this.defaultFilePath = null;
     }
 
     // Constructor for message and cause
+    // Конструктор для message и cause
     public FileProcessingException(String message, Throwable cause) {
         super(message, cause);
         this.defaultFilePath = null;
