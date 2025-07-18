c147809e4046307e149fc6a7ee29fecd5f419d5e
sVuMPaR <145699180+sVuMPaR@users.noreply.github.com>
2025-07-18 21:07:04 +0500

    Update Decrypt.java
    Add comments

--- src/Commands/Decrypt.java
+++ src/Commands/Decrypt.java
@@ -2,11 +2,13 @@ package Commands;
 
 public class Decrypt {
     // Decrypts a string
+    // Расшифровка строки
     public static String decryptText(String text, int shift) {
         return Encrypt.encryptText(text, -shift);
     }
 
     // Decrypts a file (reads, decrypts, returns decrypted string)
+    // Расшифровавыет файл (считываает, расшифровывает, возвращает расшифрованную строку)
     public static String decryptFile(String filename, int shift) {
         // shift is positive, we negate it in the logic
         return Encrypt.encryptFile(filename, -shift);
